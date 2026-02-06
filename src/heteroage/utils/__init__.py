5c233b0c4421e5c26fc542a9fdf54302b0dafbca
Shengwei Li <lishengwei98@163.com>
2026-02-06 13:38:23 +0800

    change init file

--- src/heteroage/utils/__init__.py
+++ src/heteroage/utils/__init__.py
@@ -1,12 +1,11 @@
 from .bio_utils import (
-    load_pathway_definitions as load_hallmark_dict,  # Alias for backward compatibility
-    construct_biosparse_topology as create_tiered_hallmark_mask, # Alias: maps CLI call to new function
-    setup_logger
+    load_pathway_definitions as load_hallmark_dict,
+    construct_biosparse_topology
 )
+from .logger import setup_logger
 
-# Export these names to be accessible via 'from .utils import ...'
 __all__ = [
     'load_hallmark_dict',
-    'create_tiered_hallmark_mask', 
+    'construct_biosparse_topology',
     'setup_logger'
 ]
